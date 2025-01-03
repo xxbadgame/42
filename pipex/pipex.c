76039e688d92baa032cf5fe2c51f46e16443b497
Yannis <yannis.nzuepro@gmail.com>
2025-01-03 15:09:05 +0100

    42

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: yannis <yannis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 22:13:23 by yannis            #+#    #+#             */
-/*   Updated: 2025/01/02 00:56:02 by yannis           ###   ########.fr       */
+/*   Updated: 2025/01/03 14:38:14 by yannis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,37 @@ void    execute_cmd(char *file, char *cmd)
     char *path_bin;
     char *full_path;
     char **detail_cmd;
+    int len_opt;
+    char **args;
     size_t dest_size ;
 
+    len_opt = 0;
     detail_cmd = ft_split(cmd, ' ');
-
+    
+    while (detail_cmd[len_opt])
+        len_opt++;
+    
     path_bin = "/bin/";
     dest_size = ft_strlen(path_bin) + ft_strlen(detail_cmd[0]);
     full_path = malloc(dest_size + 1);
     ft_strlcpy(full_path, path_bin, ft_strlen(path_bin) + 1);
     ft_strlcat(full_path, detail_cmd[0], dest_size + 1);
+
+    args = malloc(len_opt * sizeof(char *));
+    int i_args = 0;
+
+    // /bin/cmd[0]
+    args[i_args++] = full_path;
+
+    // suite du split commence à 1 et dans l'ordre execve on est a 1 aussi
+    while (i_args < len_opt)
+    {
+        args[i_args] = detail_cmd[i_args];
+        i_args++;
+    }
     
-    printf("file : %s", file);
-    
-    char *args[4];
-    args[0] = full_path;
-    // uniquement si on ajoute un argument
-    args[1] = detail_cmd[1];
-    args[2] = file;
-    args[3] = NULL;
+    args[i_args++] = file;
+    args[i_args++] = NULL;
     
     if (execve(full_path, args, NULL) == -1) {
         perror("execve");
